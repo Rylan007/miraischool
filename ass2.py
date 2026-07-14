e3b86d481e3267157cd062f7a0cb637f791b4fb4
Rylan007 <162426112+Rylan007@users.noreply.github.com>
2026-07-14 20:19:34 +0530

    Update ass2.py

--- ass2.py
+++ ass2.py
@@ -3,19 +3,19 @@ from google import genai
 from dotenv import load_dotenv
 import os
 
-# Load API Key
+
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-# Page Config
+
 st.set_page_config(page_title="AI Multiverse", page_icon="🤖")
 
 st.title("🌍 AI Multiverse")
 st.write("Talk with different AI Personalities!")
 
-# Sidebar
+
 st.sidebar.title("Choose Personality")
 
 personality = st.sidebar.selectbox(
